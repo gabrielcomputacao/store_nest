779d3326baa257dc7a91fd4501ef4602e524269a
Gabriel Henrique <gabrielcomputacao@outlook.com>
2025-12-08 06:37:39 -0300

    feat: creating config to database

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,22 @@
 import { Module } from '@nestjs/common';
 import { UserModule } from './user/user.module';
 import { ProductModule } from './product/product.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { DbCOnfigService } from './config/db.config.service';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [UserModule,ProductModule], 
+  imports: [
+    UserModule,
+    ProductModule,
+     ConfigModule.forRoot({
+      isGlobal: true
+     }) ,
+    TypeOrmModule.forRootAsync({
+      useClass: DbCOnfigService,
+      inject: [DbCOnfigService],
+    }),
+  ],
   controllers: [],
   providers: [],
 })
