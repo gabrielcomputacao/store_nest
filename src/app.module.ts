bbee05ad2c2dfd4a7b40f12fb76650e7707f5c7c
Gabriel Henrique <gabrielcomputacao@outlook.com>
2025-12-23 06:04:57 -0300

    feat: creating redis in docker

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,30 +8,39 @@ import { OrderModule } from './order/order.module';
 import { APP_FILTER } from '@nestjs/core';
 import { ExceptionCustomFilter } from './filter/ExceptionFilter.filter';
 import { CacheModule } from '@nestjs/cache-manager';
+import { redisStore } from 'cache-manager-redis-yet';
 
 @Module({
   imports: [
     UserModule,
     ProductModule,
-     ConfigModule.forRoot({
-      isGlobal: true
-     }) ,
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
     TypeOrmModule.forRootAsync({
       useClass: DbCOnfigService,
       inject: [DbCOnfigService],
     }),
     OrderModule,
-    CacheModule.register({
+    CacheModule.registerAsync({
       isGlobal: true,
-      ttl:10000,
-    })
+      useFactory: async () => ({
+        store: await redisStore({
+          socket: {
+            host: 'localhost',
+            port: 6379,
+          },
+          ttl: 10 * 1000
+        }),
+      }),
+    }),
   ],
   controllers: [],
   providers: [
     {
       provide: APP_FILTER,
-      useClass: ExceptionCustomFilter
-    }
+      useClass: ExceptionCustomFilter,
+    },
   ],
 })
-export class AppModule {}
+export class AppModule { }
