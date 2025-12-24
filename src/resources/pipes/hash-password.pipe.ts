b8e19c22dfc58017443510f861d746ddc20d1061
Gabriel Henrique <gabrielcomputacao@outlook.com>
2025-12-24 05:34:29 -0300

    feat: create hash password with pipe

--- src/resources/pipes/hash-password.pipe.ts
+++ src/resources/pipes/hash-password.pipe.ts
@@ -1,8 +1,18 @@
 import { ArgumentMetadata, Injectable, PipeTransform } from '@nestjs/common';
-
+import { ConfigService } from '@nestjs/config';
+import * as bcrypt from 'bcrypt'
 @Injectable()
 export class HashPasswordPipe implements PipeTransform {
-  transform(password: string) {
-    return password + 'abcdef';
+
+  constructor(private readonly configService: ConfigService){}
+
+  async transform(password: string) {
+    
+    const salt = this.configService.get<string>('SAL_PASSWORD');
+   
+    const hashPassword = await bcrypt.hash( password ,salt!);
+   
+
+    return hashPassword;
   }
 }
